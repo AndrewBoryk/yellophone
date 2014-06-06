f99675cf63fab15863d1c46fafc7c38bf528cded
Andrew Boryk <andrewcboryk@gmail.com>
2014-06-06 13:58:58 -0400

    Initial Commit

---
+++ yellophone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yellophone
+//
+//  Created by Andrew Boryk on 6/6/14.
+//  Copyright (c) 2014 Andrew Boryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
